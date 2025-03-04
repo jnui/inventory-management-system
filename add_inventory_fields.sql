0a0f85dde883730af44d418c5a319f48c2dbbd08
jnui <johnny@niumata.org@users.noreply.github.com>
2025-03-04 12:20:11 -0500

    remove old sql files

--- add_inventory_fields.sql
+++
@@ -1,7 +0,0 @@
--- Add new fields to inventory_change_entries table
-ALTER TABLE inventory_change_entries 
-ADD COLUMN items_added INT DEFAULT 0 COMMENT 'Number of items added to inventory',
-ADD COLUMN items_removed INT DEFAULT 0 COMMENT 'Number of items removed from inventory';
-
--- Update existing records to have default values
-UPDATE inventory_change_entries SET items_added = 0, items_removed = 0; 
